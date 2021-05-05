637b9d34363ba358e661e44e0dea3c6d7826f4ff
sergey <faldet11@gmail.com>
2021-05-05 15:36:06 +0500

    init

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const timtout = (ms) =>
     }, ms);
   });
 
+const interwal = (ms) =>
+  new Promise((resolve, reject) => {
+    setInterval(() => {
+      resolve();
+      console.log("INTERWAL");
+    }, ms);
+  });
+
 const main = async () => {
+  await interwal(100, 800);
   await timtout(500);
   const json = await fs.readFile("example.json");
   console.log(json);
